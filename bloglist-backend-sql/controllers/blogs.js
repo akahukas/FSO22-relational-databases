e479317cd33cecc8e87d774013f29dbe1d2cd961
Saku Hakamäki <saku.hakamaki66@gmail.com>
2022-09-17 15:11:33 +0300

    Adds Structuring code -task. (13.5)

---
+++ bloglist-backend-sql/controllers/blogs.js
@@ -0,0 +1,41 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+const { blogFinder } = require('../util/middleware')
+
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    console.log(req.body)
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    return res.json(blog)
+  }
+  else {
+    return res.status(404).end()
+  }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await Blog.destroy({ where: { id: req.params.id } })
+    return res.status(204).end()
+  }
+  else {
+    return res.status(404).end()
+  }
+})
+
+module.exports = router
