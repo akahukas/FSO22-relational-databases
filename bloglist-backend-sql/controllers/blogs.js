5966a61227618a9cd6b3e8c07c4589aeed8b4898
Saku Hakamäki <saku.hakamaki66@gmail.com>
2022-09-18 13:06:20 +0300

    Adds Include blogs in users-route and vice versa -task. (13.12)

--- bloglist-backend-sql/controllers/blogs.js
+++ bloglist-backend-sql/controllers/blogs.js
@@ -5,7 +5,13 @@ const { blogFinder, tokenExtractor, userExtractor } = require('../util/middlewar
 
 
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ['userId'] },
+    include: {
+      model: User,
+      attributes: ['name']
+    }
+  })
   res.json(blogs)
 })
 
