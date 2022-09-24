000034b592039d6be9917ce48e8a1b6a93714a2a
Saku Hakamäki <saku.hakamaki66@gmail.com>
2022-09-24 09:08:53 +0300

    Adds Readinglists on single user-view -task. (13.21)

--- bloglist-backend-sql/controllers/users.js
+++ bloglist-backend-sql/controllers/users.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
       as: 'markedBooks',
       attributes: { exclude: ['userId', 'blogId', 'createdAt', 'updatedAt'] },
       through: {
-        attributes: []
+        attributes: ['read', 'id']
       }
     }
   })
